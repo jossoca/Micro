8758402ebbda1e6ef1109e6667af7212f89b12f1
jossoca <36978755+jossoca@users.noreply.github.com>
2022-02-23 15:09:48 -0600

    Add files via upload

--- xyz_key.py
+++ xyz_key.py
@@ -8,21 +8,44 @@ import sys
 from numpy import array, floor
 import pygame
 import copy
+from subprocess import check_output
 
 
-#GLOBAL VARS
+#-----GLOBAL VARS-----
+
 pulse = 1/500
 path = '/home/pi/Documents/Database/'
 ms = [1, 1, 1]
 xyz = array([0, 0, 0])
-xy = array([186, 74])
-start_stop = 0
+vid_start_stop = 0
+
+
+display_res  = list(map(int, str(check_output("xrandr  | grep \* | cut -d' ' -f4", shell=True))[2:-3].split('x')))
+
+res_full_sensor = [4056, 3040]
+aspect_ratio_full_sensor = 4056/3040
+
+res_video = [1920, 1080]
+aspect_ratio_video = 1920/1080
+
+y_res = display_res[1] - int(11*display_res[1]/50)
+prev_window_size = [int(y_res*aspect_ratio_full_sensor), y_res]
+video_window_size = [int(y_res*aspect_ratio_video), y_res]
+
+prev_window_pos = [int((display_res[0]-prev_window_size[0])/2), int(11*display_res[1]/50)]
+video_window_pos = [int((display_res[0]-video_window_size[0])/2), int(11*display_res[1]/50)]
+
+red= (255, 0, 0)
+yellow = (255, 255, 0)
+sample_aspect_ratio = 5/2.2
+ratio_steps_pad = 2000/375
 
 screen = None
 xyz_ini = None
 
 
-#PINS DEFINITION
+#-----PINS DEFINITION-----
+
 led = 13
 
 step_gpio = 17
@@ -37,14 +60,15 @@ ms2 = 10
 ms3 = 11
 
 
-#SETUP RASP, GPIOS AND CAMERA
+#-----SETUP RASP, GPIOS AND CAMERA-----
 
 GPIO.setmode(GPIO.BCM)
 
 camera = PiCamera()
-time.sleep(1)
-camera.resolution = (4056, 3040)
-camera.start_preview(fullscreen=False, window=(132,195,760,570))
+time.sleep(0.5)
+camera.resolution = (res_full_sensor[0], res_full_sensor[1])
+camera.start_preview(fullscreen=False, window=(prev_window_pos[0], prev_window_pos[1],
+                                               prev_window_size[0], prev_window_size[1]))
 
 GPIO.setup(led, GPIO.OUT)
 
@@ -60,10 +84,10 @@ GPIO.setup(ms2, GPIO.OUT)
 GPIO.setup(ms3, GPIO.OUT)
 
 pygame.init()
-os.environ['SDL_VIDEO_WINDOW_POS']='%i,%i' % (325,5)
+os.environ['SDL_VIDEO_WINDOW_POS']='%i,%i' % (int((display_res[0] - display_res[0]/3.84)/2),0)
 
 
-#FUNCTIONS
+#-----FUNCTIONS-----
 
 def lights_on_off():
     light = GPIO.input(led)
@@ -141,12 +165,12 @@ def motor_right():
 def motor_front():
     
     global xyz
-    xyz += [0, 1, 0]
+    xyz -= [0, 1, 0]
     
     GPIO.output(enable_motX, 1)
     GPIO.output(enable_motY, 0)
     GPIO.output(enable_motZ, 1)
-    GPIO.output(dir_gpio, 0)
+    GPIO.output(dir_gpio, 1)
     step()
     return()
 
@@ -154,12 +178,12 @@ def motor_front():
 def motor_back():
     
     global xyz
-    xyz -= [0, 1, 0]
+    xyz += [0, 1, 0]
     
     GPIO.output(enable_motX, 1)
     GPIO.output(enable_motY, 0)
     GPIO.output(enable_motZ, 1)
-    GPIO.output(dir_gpio, 1)
+    GPIO.output(dir_gpio, 0)
     step()
     return()
 
@@ -223,16 +247,17 @@ def capture():
 
 def vid_capture():
     
-    global start_stop, camera
+    global vid_start_stop, camera
     
-    if start_stop == 0:
+    if vid_start_stop == 0:
         
         camera.stop_preview()
-        camera.resolution = (1920, 1080)
-        sleep(1)
-        camera.start_preview(fullscreen=False, window=(132,195,760,570))
+        camera.resolution = (res_video[0], res_video[1])
+        sleep(0.5)
+        camera.start_preview(fullscreen=False, window=(video_window_pos[0], video_window_pos[1],
+                                               video_window_size[0], video_window_size[1]))
         
-        start_stop = 1
+        vid_start_stop = 1
         
         if len(os.listdir(path + "../Videos/")) > 0:
             count = max(photos_numbering(path + "../Videos/"))
@@ -241,57 +266,77 @@ def vid_capture():
         else:
             camera.start_recording(path + "../Videos/" + 'vid1.h264')
    
-    elif start_stop == 1:
+    elif vid_start_stop == 1:
         
         camera.stop_recording()
         
         camera.stop_preview()
-        camera.resolution = (4056, 3040)
-        camera.start_preview(fullscreen=False, window=(132,195,760,570))
+        camera.resolution = (res_full_sensor[0], res_full_sensor[1])
+        camera.start_preview(fullscreen=False, window=(prev_window_pos[0], prev_window_pos[1],
+                                               prev_window_size[0], prev_window_size[1]))
 
-        start_stop =0
+        vid_start_stop =0
         
     return()
 
 
 def tracker():
     
-    global xy, xyz, xyz_ini, screen
+    global xyz, xyz_ini, screen
     
+    xy = [0, 0]
     xyz_ini = copy.copy(xyz)[[0,1]]
     
-    screen = pygame.display.set_mode((375, 150))
-    pygame.draw.rect(screen, (255, 0, 0), (xy[0], xy[1], 3, 2))
+    screen = pygame.display.set_mode((int(display_res[0]/3.84), int(display_res[0]/(3.84*sample_aspect_ratio))))
+    pygame.draw.rect(screen, red, (xy[0], xy[1], 3, 2))
     pygame.display.update()
     return()
 
 
-def tracker_update():
+def history_tracker():
     
-    global xy, xyz, xyz_ini
-   
+    global xyz, xyz_ini
+    
     try:
-        xy_update = xy + floor((xyz[[0,1]] - xyz_ini)/5.3333)
-        screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, (255, 0, 0), (xy_update[0], xy_update[1], 3, 2))
+        xy_update = floor((xyz[[0,1]] - xyz_ini)/ratio_steps_pad)
+        pygame.draw.rect(screen, yellow, (xy_update[0], xy_update[1], 3, 2))
         pygame.display.update()
     except:
         pass
     return()
 
 
+def tracker_update():
+    
+    global xyz, xyz_ini
+    
+    try:
+        xy_update = floor((xyz[[0,1]] - xyz_ini)/ratio_steps_pad)
+#         screen.fill((0, 0, 0))
+        pygame.draw.rect(screen, red, (xy_update[0], xy_update[1], 3, 2))
+        pygame.display.update()
+        print(xy)
+    except:
+        pass
+    return()
+
+
 def on_press(key):
-    global xy, screen
+
     if key == keyboard.Key.left:
+        history_tracker()
         motor_left()
         tracker_update()
     elif key == keyboard.Key.right:
+        history_tracker()
         motor_right()
         tracker_update()
     elif key == keyboard.Key.up:
+        history_tracker()
         motor_front()
         tracker_update()
     elif key == keyboard.Key.down:
+        history_tracker()
         motor_back()
         tracker_update()
     elif key == keyboard.KeyCode.from_char('x'):
@@ -318,8 +363,7 @@ def on_press(key):
         lights_on_off()
     elif key == keyboard.KeyCode.from_char('t'):
         tracker()
-    elif key == keyboard.KeyCode.from_char('v'):
-        
+    elif key == keyboard.KeyCode.from_char('v'):     
         vid_capture()
 
 
@@ -332,6 +376,7 @@ def on_release(key):
         micro_steps_off()
     elif key == keyboard.KeyCode.from_char('0'):
         micro_steps_off()
+
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
